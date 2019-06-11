74d4d00270c2cccf70a776c7a524754e340b5bca
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2019-06-11 12:07:46 +0200

    extract some image data to display to user

--- src/imgDriver.py
+++ src/imgDriver.py
@@ -8,13 +8,22 @@ import matplotlib.pyplot as plt
 def main():
     global image_data_root
     image_data_root = sys.argv[1]
+
+    image_paths = get_image_paths()
+
+
+def get_image_paths():
     print("Getting manifest.hd5 at: %s/manifest.md5" % image_data_root)
 
     manifest = pd.read_hdf("%s/manifest.h5" % image_data_root)
     image_paths = list(manifest["annotated_image_path"])
     print("Success, retrieved %d image paths" % len(image_paths))
+    print("Layers included in image: %s" % list(get_annotated_image(image_paths[0])["georef_img"]["layers"]))
 
-    test = get_annotated_image(image_paths[0])
+    test = list(get_annotated_image(image_paths[0])["georef_img"]["layers"]['visible']['array'])
+    print("Dimensions: %s x %s" %(len(test[0]), len(test)))
+
+    return image_paths
 
 
 def get_annotated_image(image_path):
