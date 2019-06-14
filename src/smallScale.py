be35bcb2ad439018b6956082cb301a8fb6d250ff
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2019-06-14 09:57:44 +0200

    refactor saving of nmp array, add HSI transform

--- src/smallScale.py
+++ src/smallScale.py
@@ -12,9 +12,22 @@ def lab_transform(image):
 
     # Convert to lab
     lab_image = color.rgb2lab(rgb_image)
+    save_nmp_array(image, lab_image, 'lab')
 
+
+def hsi_transform(image):
+    # Get the raw RGB values from the hdf5 image
+    rgb_image = (list(image["georef_img"]["layers"]['visible']['array']))
+    rgb_image = np.asarray(rgb_image)
+
+    # Convert to HSI - Note this saves H as a fraction, instead of degrees
+    hsi_image = color.rgb2hsv(rgb_image)
+    save_nmp_array(image, hsi_image, 'hsi')
+
+
+def save_nmp_array(hd5image, new_image, folder):
     # Get relative path for output directory, minus the file extension
-    rel_path = relpath(image.filename)[6:-3]
+    rel_path = relpath(hd5image.filename)[6:-3]
 
     # Work out which parent directories need to be created before writing the file
     directory_to_make = re.search(r'^([0-9a-zA-Z/ ]*/[0-9a-zA-Z\-]*/[0-9a-zA-Z\-]*)/[a-zA-Z0-9]*$', rel_path).group(1)
@@ -23,11 +36,8 @@ def lab_transform(image):
         raise Exception("Invalid directory path")
 
     # Make the parent directories
-    makedirs("../img/lab/%s" % directory_to_make, exist_ok=True)
+    makedirs("../img/%s/%s" % (folder, directory_to_make), exist_ok=True)
 
     # Save the Lab image as a numpy array to preserve accuracy - Tensorflow will need to read in these images with numpy
-    np.save("../img/lab/%s%s" % (rel_path, '.npy'), lab_image)
-
-
-def hsi_transform(image):
-    pass
+    # We will also need a way of either saving the tree masks, or retrieving them from the original image
+    np.save("../img/%s/%s%s" % (folder, rel_path, '.npy'), new_image)
