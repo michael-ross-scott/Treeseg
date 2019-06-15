9f1737fda56f94d7463f063cdf18a0aa3fcff434
Charl Ritter <CharlRitter@users.noreply.github.com>
2019-06-15 15:15:37 +0200

    Easier implementation of edge detector

--- src/medScale.py
+++ src/medScale.py
@@ -1,129 +1,23 @@
 #CANNY EDGE DETECTOR IMPORTS
-
-#import matplotlib.pyplot as plt
+import cv2
 import numpy as np
-#import matplotlib.image as mpimg
-#import os
-#import scipy.misc as sm
-#import skimage
-from scipy import ndimage
+from matplotlib import pyplot as plt
 
-#CANNY EDGE DETECTOR IMPORTS
-
+#MEAN SHIFT IMPORTS
 
 
 def mean_shift_transform(image):
     pass
 
+########### MEAN SHIFT METHODS ###########
 
 def edge_detector_transform(image):
-    image_filtered = convolve(image, gaussian_kernel(5, sigma=1.4))
-    grad, theta = sobel_filters(image_filtered)
-    image_nms = non_max_suppression(grad, theta)
-    image_thresh, weak, strong = threshold(image_nms, lowThresholdRatio=0.07, highThresholdRatio=0.19)
-    image_final = hysteresis(image_thresh, weak, strong=strong)
+    image = cv2.imread('C:/Users/charl_6lfr58n/Desktop/2.jpg', 0)
+    edges = cv2.Canny(image, 100, 200)
 
-    return image_final
+    plt.subplot(121), plt.imshow(image, cmap='gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(edges, cmap='gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-########### CANNY EDGE DETECTOR DEFS ###########
-# Step 1 - Noise reduction
-def gaussian_kernel(size, sigma=1):
-    size = int(size) // 2
-    x, y = np.mgrid[-size:size + 1, -size:size + 1]
-    normal = 1 / (2.0 * np.pi * sigma ** 2)
-    g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2))) * normal
-    return g
-
-# Step 2 - Gradient calculation
-def sobel_filters(img):
-    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
-
-    Ix = ndimage.filters.convolve(img, Kx)
-    Iy = ndimage.filters.convolve(img, Ky)
-
-    G = np.hypot(Ix, Iy)
-    G = G / G.max() * 255
-    theta = np.arctan2(Iy, Ix)
-
-    return (G, theta)
-
-# Step 3 - Non - maximum suppression
-def non_max_suppression(img, D):
-    M, N = img.shape
-    Z = np.zeros((M, N), dtype=np.int32)
-    angle = D * 180. / np.pi
-    angle[angle < 0] += 180
-
-    for i in range(1, M - 1):
-        for j in range(1, N - 1):
-            try:
-                q = 255
-                r = 255
-
-                # angle 0
-                if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
-                    q = img[i, j + 1]
-                    r = img[i, j - 1]
-                # angle 45
-                elif (22.5 <= angle[i, j] < 67.5):
-                    q = img[i + 1, j - 1]
-                    r = img[i - 1, j + 1]
-                # angle 90
-                elif (67.5 <= angle[i, j] < 112.5):
-                    q = img[i + 1, j]
-                    r = img[i - 1, j]
-                # angle 135
-                elif (112.5 <= angle[i, j] < 157.5):
-                    q = img[i - 1, j - 1]
-                    r = img[i + 1, j + 1]
-
-                if (img[i, j] >= q) and (img[i, j] >= r):
-                    Z[i, j] = img[i, j]
-                else:
-                    Z[i, j] = 0
-
-            except IndexError as e:
-                pass
-
-    return Z
-
-# Step 4 - Double threshold
-def threshold(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
-
-    highThreshold = img.max() * highThresholdRatio;
-    lowThreshold = highThreshold * lowThresholdRatio;
-
-    M, N = img.shape
-    res = np.zeros((M, N), dtype=np.int32)
-
-    weak = np.int32(25)
-    strong = np.int32(255)
-
-    strong_i, strong_j = np.where(img >= highThreshold)
-    zeros_i, zeros_j = np.where(img < lowThreshold)
-
-    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
-
-    res[strong_i, strong_j] = strong
-    res[weak_i, weak_j] = weak
-
-    return res, weak, strong
-
-# Step 5 - Edge Tracking by Hysteresis
-def hysteresis(img, weak, strong=255):
-    M, N = img.shape
-    for i in range(1, M - 1):
-        for j in range(1, N - 1):
-            if img[i, j] == weak:
-                try:
-                    if ((img[i + 1, j - 1] == strong) or (img[i + 1, j] == strong) or (img[i + 1, j + 1] == strong)
-                            or (img[i, j - 1] == strong) or (img[i, j + 1] == strong)
-                            or (img[i - 1, j - 1] == strong) or (img[i - 1, j] == strong) or (
-                                    img[i - 1, j + 1] == strong)):
-                        img[i, j] = strong
-                    else:
-                        img[i, j] = 0
-                except IndexError as e:
-                    pass
-    return img
+    plt.show()
