247ee7b3ab7257c646b8571d34d361f315df59c7
Charl Ritter <charlritter@hotmail.com>
2019-07-17 11:23:19 +0200

    Update imgDriver.py

--- src/imgDriver.py
+++ src/imgDriver.py
@@ -3,14 +3,14 @@ import pandas as pd
 import h5py
 import datetime
 
-from src import smallScale, medScale, lrgScale
+from src import smallScale, medScale
 
 
 def main():
     global image_data_root
     image_data_root = sys.argv[1]
 
-    transforms = sys.argv[2:]
+    transforms = sys.argv[2]
     print("Performing transforms for: %s" % transforms)
 
     image_paths = get_image_paths()
@@ -20,19 +20,22 @@ def main():
     for image_path in image_paths:
         i+=1
         image = get_annotated_image(image_path)
-
         if 'lab' in transforms:
             smallScale.lab_transform(image)
         if 'hsi' in transforms:
             smallScale.hsi_transform(image)
+        if 'hsl' in transforms:
+            smallScale.hsl_transform(image)
+        if 'pca' in transforms:
+            smallScale.pca_transform(image)
+        if 'ica' in transforms:
+            smallScale.ica_transform(image)
+        if 'hist_equal' in transforms:
+            medScale.hist_equal_transform(image)
         if 'mean_shift' in transforms:
             medScale.mean_shift_transform(image)
         if 'edge_detector' in transforms:
             medScale.edge_detector_transform(image)
-        if 'hough' in transforms:
-            lrgScale.hough_transform(image)
-        if 'template' in transforms:
-            lrgScale.template_transform(image)
         print("Completed image %d" % i)
     print("Time Taken: %ss" % (round((datetime.datetime.now() - start_time).total_seconds())))
 
@@ -45,7 +48,7 @@ def get_image_paths():
     print("Layers included in image: %s" % list(get_annotated_image(image_paths[0])["georef_img"]["layers"]))
 
     test = list(get_annotated_image(image_paths[0])["georef_img"]["layers"]['visible']['array'])
-    print("Dimensions: %s x %s" %(len(test[0]), len(test)))
+    print("Dimensions: %s x %s" % (len(test[0]), len(test)))
 
     return image_paths
 
