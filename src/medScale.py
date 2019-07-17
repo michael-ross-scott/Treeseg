9c02fa7cc8de24d8ec0d3d72f2cc8b5f51412525
Charl Ritter <charlritter@hotmail.com>
2019-07-17 22:38:41 +0200

    New edge detector. All there, but not working yet

--- src/medScale.py
+++ src/medScale.py
@@ -3,32 +3,29 @@ import numpy as np
 from os.path import relpath
 from os import makedirs
 import pymeanshift as pms
-from pylab import *
+
+from feature_extractors.canny_edge_detector import canny_edge_detector as ced
 
 def mean_shift_transform(image):
     # Get the raw RGB values from the hdf5 image
-    rgb_image = (list(image["georef_img"]["layers"]['visible']['array']))
-    rgb_image = np.asarray(rgb_image)
+    ndvi_image = (list(image["georef_img"]["layers"]['ndvi']['array']))
+    np_image = np.asarray(ndvi_image)
 
     # Convert to Mean Shift
-    (mean_shift_image, labels_image, number_regions) = pms.segment(rgb_image, spatial_radius=3, range_radius=4.5, min_density=300)
+    (mean_shift_image, labels_image, number_regions) = pms.segment(np_image, spatial_radius=3, range_radius=4.5, min_density=300)
     save_nmp_array(image, mean_shift_image, 'mean_shift')
 
 def edge_detector_transform(image):
     # Get the raw RGB values from the hdf5 image
-    rgb_image = (list(image["georef_img"]["layers"]['nir']['array']))
-    # v = np.median(rgb_image)
-    rgb_image = np.asarray(rgb_image)
+    nir_image = (list(image["georef_img"]["layers"]['nir']['array']))
+    np_image = np.asarray(nir_image)
 
-    #Convert to Canny Edge Detector
-    higher_threshold = 275
-    lower_threshold = 230
-    # sigma = 0.33
-    # lower_threshold = int(max(0, (1.0 - sigma) * v))
-    # higher_threshold = int(min(255, (1.0 + sigma) * v))
-    aperture = 3
+    detector = ced.cannyEdgeDetector(np_image, sigma=1.4, kernel_size=5, lowthreshold=0.09, highthreshold=0.17,
+                                     weak_pixel=100)
+    edge_detector_image = detector.detect()
 
-    edge_detector_image = cv2.Canny(rgb_image, lower_threshold, higher_threshold, aperture)
+    # edge_detector_image = cv2.Canny(np_image,10,255,7,L2gradient=True)
+
     save_nmp_array(image, edge_detector_image, 'edge_detector')
 
 # Performs histogram equalization on nir
