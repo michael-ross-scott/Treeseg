66f31031a0e4e18299d9216937afcc37e2aec549
Charl Ritter <CharlRitter@users.noreply.github.com>
2019-07-18 12:44:53 +0200

    Given up on making canny better, openCV seems to be best

--- src/medScale.py
+++ src/medScale.py
@@ -4,8 +4,6 @@ from os.path import relpath
 from os import makedirs
 import pymeanshift as pms
 
-from feature_extractors.canny_edge_detector import canny_edge_detector as ced
-
 def mean_shift_transform(image):
     # Get the raw RGB values from the hdf5 image
     ndvi_image = (list(image["georef_img"]["layers"]['ndvi']['array']))
@@ -15,16 +13,19 @@ def mean_shift_transform(image):
     (mean_shift_image, labels_image, number_regions) = pms.segment(np_image, spatial_radius=3, range_radius=4.5, min_density=300)
     save_nmp_array(image, mean_shift_image, 'mean_shift')
 
-def edge_detector_transform(image):
+def edge_detector_transform(image, sigma=0.33):
     # Get the raw RGB values from the hdf5 image
     nir_image = (list(image["georef_img"]["layers"]['nir']['array']))
     np_image = np.asarray(nir_image)
 
-    detector = ced.cannyEdgeDetector(np_image, sigma=1.4, kernel_size=5, lowthreshold=0.09, highthreshold=0.17,
-                                     weak_pixel=100)
-    edge_detector_image = detector.detect()
+    # compute the median of the single channel pixel intensities
+    v = np.median(nir_image)
 
-    # edge_detector_image = cv2.Canny(np_image,10,255,7,L2gradient=True)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+
+    edge_detector_image = cv2.Canny(np_image,lower,upper)
 
     save_nmp_array(image, edge_detector_image, 'edge_detector')
 
